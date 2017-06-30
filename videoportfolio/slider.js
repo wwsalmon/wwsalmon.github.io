291a0cb635f0ef9243d45f760f491102e46a5566
wwsalmon <wwsamson@yahoo.com>
2017-06-30 14:27:15 -0400

    Update to Video Portfolio 6/30

--- videoportfolio/slider.js
+++ videoportfolio/slider.js
@@ -32,6 +32,8 @@ function showDivs(n) {
 
 */
 
+/*
+
 console.clear();
 
 (function($, sliderId) { // closure for this 'plugin'
@@ -98,3 +100,49 @@ console.clear();
 // think about it like this ^ ... - they take the place of the parameters inside the function.
 
 $('.slides-container').initializeSlider();
+
+*/
+
+var slideIndex = 1;
+
+function showDivs(n,el) {
+
+	console.log("showing div");
+
+  // Get the right container from the passed element.
+  var container = el.closest(".slides-container");
+
+  // Find the child elements.
+  var x = container.find(".slides-main");
+  var menu = container.find(".slides-menu");
+  var dots = container.find(".slides-picker");
+
+  var menuleft = 0;
+  var i;
+
+  // Calculate the new slideIndex.
+  slideIndex += n;
+  if (slideIndex > x.length) {slideIndex = 1}
+  if (slideIndex < 1) {slideIndex = x.length}
+
+  // Animate.
+  x.fadeOut(200);
+  dots.css("opacity",0.2);
+  x.eq(slideIndex-1).fadeIn(200);
+  dots.eq(slideIndex-1).css("opacity",1);
+  menuleft = dots.eq(slideIndex-1).position().left;
+  menu.animate({scrollLeft: menuleft},200);
+}
+
+// Click handlers
+$(".slides-next").on("click",function(){
+  showDivs(1,$(this));
+});
+
+$(".slides-back").on("click",function(){
+  showDivs(-1,$(this));
+});
+
+$(".slides-picker-outer").on("click",function(){
+  showDivs($(this).index()+1-slideIndex,$(this));
+});
