f7f1cd9490426f4c403ac90cb10b862dac09897e
wwsalmon <wwsamson@yahoo.com>
2017-07-13 16:26:31 -0400

    Begin Migration to wwsalmon.com

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # About this website
 
 This website is the personal website of Samson Zhang, hosted on Github because my fifty other websites weren't enough.
+
+As of this commit, the migration to wwsalmon.com has started.
--- videoportfolio/slider.js
+++ videoportfolio/slider.js
@@ -1,115 +1,7 @@
-/*
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-    showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-    var menuleft = 0;
-    var x = $(".slides-main");
-    var i;
-    var menu = $(".slides-menu");
-    var dots = $(".slides-picker-outer");
-    x.hide();
-    dots.css("opacity","0.2");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
-    for (i = 0; i < x.length; i++) {
-       x.eq(i).fadeOut(200);
-    }
-    x.eq(slideIndex-1).fadeIn(200);
-    dots.eq(slideIndex-1).css("opacity","1.0");
-    menuleft = dots.eq(slideIndex-1).position().left;
-    menu.animate({scrollLeft: menuleft},200);
-}
-
-*/
-
-/*
-
-console.clear();
-
-(function($, sliderId) { // closure for this 'plugin'
-
-	//   name of plugin - used as a method on a DOM element
-	$.fn.initializeSlider = function() {
-
-    return this.each( function() { // runs a function for each slider - which creates seperate places for all the stuff memory - for each slider in this case
-      sliderId++; // something to increment so you can name each slider for example
-
-      var sliderName = 'Slider-' + sliderId;
-      var slideIndex = 1;
-      var menuLeft = 0;
-      var $this = $(this);
-
-      $back = $this.find('.slides-back');
-      $next = $this.find('.slides-next');
-      $dot = $this.find('.slides-picker');
-      $menu = $this.find('.slides-menu');
-      $main = $this.find('.slides-main');
-
-      checkSlide();
-
-      // whatever your methods are for a slider
-      $back.on('click', function() {
-        buttonClick(-1);
-      });
-
-      $next.on('click', function() {
-        buttonClick(1);
-      });
-
-      $dot.on('click', function() {
-        var clickedSlide = $this.find('.slides-picker').index($(this)) + 1;
-        slideIndex = clickedSlide;
-        checkSlide();
-      });
-
-      function buttonClick(n){
-        slideIndex += n;
-        if(slideIndex > $main.length){slideIndex=1;}
-        if(slideIndex < 1){slideIndex=$main.length;}
-        checkSlide();
-      }
-
-      function checkSlide(){
-        $main.fadeOut(200);
-        $dot.css("opacity","0.2");
-        $main.eq(slideIndex-1).fadeIn(200);
-        $dot.eq(slideIndex-1).css("opacity","1.0");
-        menuleft = $dot.eq(slideIndex-1).position().left;
-        $menu.animate({scrollLeft: menuleft},200);
-        //alert("checking slide" + sliderId);
-      }
-
-      // just showing that multiple instances of this slider are initialized
-      console.log('You created slider #' + sliderId);
-    });
-
-  }
-
-})(jQuery, 0); // these things at the end are arguments 'passed' in to the larger function...
-// ourWholeFunction(jQuery, 0);
-// think about it like this ^ ... - they take the place of the parameters inside the function.
-
-$('.slides-container').initializeSlider();
-
-*/
-
-var slideIndex = 1;
+var slideIndex = 0;
 
 function showDivs(n,el) {
 
-	console.log("showing div");
-
-  // Get the right container from the passed element.
   var container = el.closest(".slides-container");
 
   // Find the child elements.
@@ -120,12 +12,10 @@ function showDivs(n,el) {
   var menuleft = 0;
   var i;
 
-  // Calculate the new slideIndex.
   slideIndex += n;
   if (slideIndex > x.length) {slideIndex = 1}
   if (slideIndex < 1) {slideIndex = x.length}
 
-  // Animate.
   x.fadeOut(200);
   dots.css("opacity",0.2);
   x.eq(slideIndex-1).fadeIn(200);
@@ -134,7 +24,6 @@ function showDivs(n,el) {
   menu.animate({scrollLeft: menuleft},200);
 }
 
-// Click handlers
 $(".slides-next").on("click",function(){
   showDivs(1,$(this));
 });
