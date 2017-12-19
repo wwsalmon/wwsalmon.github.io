377b9c88a00de097bf0e288d435a93b4a6155050
wwsalmon <wwsamson@yahoo.com>
2017-12-18 21:51:04 -0500

    Case Study Prototype

---
+++ header2.js
@@ -0,0 +1,10 @@
+$("head").append(
+`
+<link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'/>
+<link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i|Roboto:300,300i,400,400i,500,500i,700,700i' rel='stylesheet'/>
+<link rel='stylesheet' type='text/css' href='style2.css'>
+<link rel='stylesheet' type='text/css' href='fonts.css'/>
+<link rel='stylesheet' type='text/css' href='../fonts.css'/>
+<meta content='width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, target-densityDpi=device-dpi' name='viewport'/>
+`
+)
