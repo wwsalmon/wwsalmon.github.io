df416a2238c7f042903efa26702e5281d1c963a1
wwsalmon <wwsamson@yahoo.com>
2017-12-19 22:55:40 -0500

    P6 Case Study

--- header.js
+++ header.js
@@ -7,6 +7,8 @@ $("head").append(
 <link rel='stylesheet' type='text/css' href='fonts.css'/>
 <link rel='stylesheet' type='text/css' href='../style.css'>
 <link rel='stylesheet' type='text/css' href='../fonts.css'/>
+<link rel='stylesheet' type='text/css' href='cocoen.min.css'/>
+<link rel='stylesheet' type='text/css' href='../cocoen.min.css'/>
 
 <meta content='width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, target-densityDpi=device-dpi' name='viewport'/>
 `
