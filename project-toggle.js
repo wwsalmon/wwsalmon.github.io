727009ec9bcc76bcd4fb01a0b9cba62bd576236f
Samson Zhang <wwsamson@yahoo.com>
2018-06-18 00:05:11 -0400

    marchforourlives case study, revamped home cat tabs

--- project-toggle.js
+++ project-toggle.js
@@ -1,15 +1,22 @@
 $(function(){
-  $(".projects-photo").fadeOut();
+  $(".projects").fadeOut(0);
+  changeTab()
 });
 
-function changeTab(category){
-  if (category == "photo"){
+$(".home-tabs input").on('change', function(){
+  changeTab();
+})
+
+function changeTab(){
+  category = $(".home-tabs input[name=home-tabs]:checked").attr("id");
+  console.log(category);
+  if (category == "cat-photo"){
     $(".cat-video").removeClass("selected");
     $(".cat-photo").addClass("selected");
     $(".projects-video").fadeOut(100);
     $(".projects-photo").fadeIn(200);
   }
-  if (category == "video"){
+  if (category == "cat-video"){
     $(".cat-photo").removeClass("selected");
     $(".cat-video").addClass("selected");
     $(".projects-photo").fadeOut(100);
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
-# Video Portfolio 2
+# wwsalmon.github.io
+*my personal website/portfolio*
 
-In name, the second version of my [video portfolio](wwsalmon.com/videoportfolio), in purpose set to eventually be my main website.
+This repo hosts my personal website, a video/photo portfolio hosted at [wwsalmon.com](http://www.wwsalmon.com). The current live website was prototype version 0.4.5, aka videoportfolio2.
+
+Under /archive, it also hosts a little documentation of previous versions of wwsalmon.com, including a detailed commentary and lightly modified prototypes from 0.3.3 to 0.4.4.
